7d418ba5f861e0859e2170aec76097fafd95907b
ystt-lita <62131012+ystt-lita@users.noreply.github.com>
2021-02-22 23:26:12 +0900

    Update AppSettings.cs

--- Scripts/AppSettings.cs
+++ Scripts/AppSettings.cs
@@ -2,21 +2,21 @@ namespace WYUN
 {
     public class AppSettings
     {
-        public string server { get; set; }
+        public string ip { get; set; }
         public int port { get; set; }
         public long appID { get; set; }
         public string userName { get; set; }
 
         public AppSettings(string ip_port, long id, string name)
         {
-            server = ip_port.Split(':')[0];
+            ip = ip_port.Split(':')[0];
             port = int.Parse(ip_port.Split(':')[1]);
             appID = id;
             userName = name;
         }
         public AppSettings(string ip, int p, long id, string name)
         {
-            server = ip;
+            ip = ip;
             port = p;
             appID = id;
             userName = name;
